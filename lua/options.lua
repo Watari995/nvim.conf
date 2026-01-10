37f3d1ff895dd7ba62d2cce10bf4f815f58e1a70
Wataru Ichimura <wata_2@icloud.com>
2026-01-11 03:00:10 +0900

    update

--- lua/options.lua
+++ lua/options.lua
@@ -67,4 +67,15 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- 外部でファイルが変更されたときに自動で再読み込み
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  callback = function()
+    if vim.fn.mode() ~= 'c' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
+
 -- vim: ts=2 sts=2 sw=2 et
